814a28c025297ece1b739da8dea6294d9a756e99
arunkumark-ship-it <arunkumark@ninjacart.com>
2026-05-24 23:12:41 +0530

    1

--- app.py
+++ app.py
@@ -1017,8 +1017,14 @@ def page_sales():
             co_city=st.selectbox("City *",
                 ["Bengaluru","Mysuru","Hubli","Mangaluru","Hassan","Tumkur"],key="co_city")
             co_cls =st.selectbox("Classification",
-                ["A","B","C","Premium","Wholesale","Retail"],key="co_cls")
-            co_addr=st.text_input("Shop address *",
+                ["Restaurants","PG","Pubs","Premium Hotels","Wholesale","Retail"],key="co_cls")
+            if co_Cls == "Other":
+                co_cls_Other = st.text_input("Enter Classification Name",
+                placeholder="Type your Classification here",
+                key="Co_cls_Other")
+            else:
+                co_cls_Other =co_cls
+            co_addr=st.text_input("Shop address",
                 placeholder="e.g. 12/3 MG Road, Bengaluru",
                 key="co_addr",
                 help="Type the shop address — plain text, no map search needed")
